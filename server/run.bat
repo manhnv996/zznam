d155f3f0311c66a0e34e032038197aca32389528
Hoang <kienhg96@gmail.com>
2017-11-25 21:59:58 +0700

    Add file to run server

---
+++ server/run.bat
@@ -0,0 +1,18 @@
+java -classpath ^
+./classes;^
+./lib/bz-allinone.jar;^
+./lib/commons-codec-1.5.jar;^
+./lib/commons-io-1.4.jar;^
+./lib/commons-lang-2.4.jar;^
+./lib/commons-logging-1.1.1.jar;^
+./lib/commons-pool-1.5.5.jar;^
+./lib/gson-2.2.4.jar;^
+./lib/httpclient_4.5.2.jar;^
+./lib/httpcore_4.4.4.jar;^
+./lib/json.jar;^
+./lib/log4j-1.2.15.jar;^
+./lib/slf4j-api-1.6.1.jar;^
+./lib/slf4j-log4j12-1.6.1.jar;^
+./lib/spymemcached.jar;^
+./lib/xstream-1.3.1.jar ^
+bitzero.server.Main
